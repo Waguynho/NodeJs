7b9b71ddc6308e320f8720146d0a1ed71763fdd2
Wagner dos Santos <wagner-power@hotmail.com>
2016-12-25 12:16:29 -0200

    Acessando Mondo  localmente - via NodeJs

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const http = require('http');
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
-var hello = require('./modulos_ws/hello.js');
+// Connection URL
+var url = 'mongodb://localhost:27017/EscolaDb';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  
+  findDocuments(db, ExibeResultado);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 204;
-  res.setHeader('Content-Type', 'application/json');
-  res.end("\"{nome:\"Wagner\",idade:27}"); 
+  db.close();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-  console.log(hello.ola());
-});
+var findDocuments = function(db, callback) {
+	  // Get the documents collection
+	  var collection = db.collection('Estudantes');
+	  // Find some documents
+	  collection.find({}).toArray(function(err, docs) {
+	    assert.equal(err, null);
+	    console.log("Achou os seguintes registros");
+	    console.log(docs)
+	    callback(docs);
+	  });
+	};
+	
+	
+	
+	function ExibeResultado(documentos){
+		
+		for (pos in documentos){
+			
+			console.log('Pessoa: '+documentos[pos].nome+' tem idade: '+documentos[pos].idade);
+			
+		}
+		
+		
+	}
+	
+	
