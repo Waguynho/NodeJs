7228b340ff11888b944af4d2ad9b231d19160a6c
Wagner dos Santos <wagner-power@hotmail.com>
2017-01-16 00:00:35 -0200

    Acessando Dados Com servidor Expreess

--- index.js
+++ index.js
@@ -1,41 +1,48 @@
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
+var express = require('express');
+var app = express();
+
 // Connection URL
 var url = 'mongodb://wagner:789789@ds139288.mlab.com:39288/carros_db';
 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-  
-  findDocuments(db, ExibeResultado);
+app.get('/carros', function (req, res) {
 
-  db.close();
+  MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Conectou ao servidor remoto");
+
+     findDocuments(db, ExibeResultado, res);
+
+
+  });
+
 });
 
-var findDocuments = function(db, callback) {
+app.listen(3000, function () {
+  console.log('Servidor escutando na porta: 3000!');
+});
+
+var findDocuments = function(db, callback, res) {
 	  // Get the documents collection
 	  var collection = db.collection('Carros');
 	  // Find some documents
-	  collection.find({}).toArray(function(err, docs) {
-	    assert.equal(err, null);
-	    console.log("Achou os seguintes registros");
-	    //console.log(docs)
-	    callback(docs);
+	    collection.find({}).toArray(function(err, docs) {
+	       assert.equal(err, null);
+	        console.log("Achou os seguintes registros");
+
+          res.json(docs);
+          //res.send();
+
+	         callback(docs);
+           db.close();
+
 	  });
-	};
-	
-	
-	
-	function ExibeResultado(documentos){
-		
-		for (pos in documentos){
-			
-			console.log('Carro: '+documentos[pos].nome+' fabricado por: '+documentos[pos].marca+', tem velocidade:'+documentos[pos].max_velo);
-			
-		}		
-		
-	}
-	
-	
+};
+
+  function ExibeResultado(documentos){
+	 documentos.forEach(function(item) {
+	  	console.log('Carro: '+item.nome + ' fabricado por: '+item.marca+', tem velocidade:'+item.max_velo);
+	});
+}
